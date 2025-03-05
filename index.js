99ead1f56f829ffd5ad196338e95f094a9daf65f
tarandeep9988 <tarandeep65singh@gmail.com>
2025-03-06 00:06:11 +0530

    added update route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const fs = require("fs");
+const cors = require("cors");
 
 // Loading environment variables
 dotenv.config();
@@ -12,6 +13,7 @@ const app = express();
 // Middlewares
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -29,16 +31,24 @@ app.get("/transactions", (req, res) => {
 }
 );
 
+// Custom middlewares
+const validateTransaction = (req, res, next) => {
+  const { title, amount, type, category } = req.body;
+  if (!title || !amount || !type || !category) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+  next(); 
+};
+
+
+
 // Adding a new transaction
-app.post("/add-transaction", (req, res) => {
+app.post("/add-transaction", validateTransaction, (req, res) => {
   const now = new Date();
   const id = now.getTime();
   const date = now.toLocaleDateString();
   const time = now.toLocaleTimeString();
   const { title, amount, type, category } = req.body;
-  if (!title || !amount || !type || !category) {
-    return res.status(400).send("Missing required fields: title, amount, type, category");
-  }
 
   fs.readFile("transactions.json", "utf8", (err, data) => {
     if (err) {
@@ -89,6 +99,43 @@ app.delete("/delete-transaction/:id", (req, res) => {
     );
   });
 }
+);
+
+app.put("/update-transaction/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, amount, type, category } = req.body;
+    
+    if (!title || !amount || !type || !category) {
+        return res.status(400).send("Missing required fields: title, amount, type, category");
+    }
+    
+    fs.readFile("transactions.json", "utf8", (err, data) => {
+        if (err) {
+        console.error(err);
+        return res.status(500).send("Error reading transactions file");
+        }
+    
+        const transactions = data ? JSON.parse(data) : [];
+        const transactionIndex = transactions.findIndex((t) => t.id === id);
+        if (transactionIndex === -1) {
+        return res.status(404).send("Transaction not found");
+        }
+    
+        transactions[transactionIndex] = { id, title, amount, type, category };
+    
+        fs.writeFile(
+        "transactions.json",
+        JSON.stringify(transactions, null, 2),
+        (err) => {
+            if (err) {
+            console.error(err);
+            return res.status(500).send("Error updating transaction");
+            }
+            res.send("Transaction updated successfully!");
+        }
+        );
+    });
+    }   
 );
 
 const PORT = process.env.PORT || 8080;
