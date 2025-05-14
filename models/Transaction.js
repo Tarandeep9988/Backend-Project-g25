dd92b680831dc9e2375dce3311058a9f73b684cc
tarandeep9988 <tarandeep65singh@gmail.com>
2025-05-14 17:43:04 +0530

    transaction Schema added

---
+++ models/Transaction.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["income", "expense"],
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Transaction', transactionSchema);
